dcebab987773088d6d5352e1953e88923878ca44
andreituta <tuta.andrei96@gmail.com>
2021-06-01 20:37:55 +0100

    Change keys and content to config

--- handler.py
+++ handler.py
@@ -6,34 +6,50 @@ from signal import SIGINT, SIGTERM, signal
 
 from datadog import statsd
 
-def make_sendinblue_message(email: str, name:str) -> None:
-
-    url = "https://api.sendinblue.com/v3/smtp/email"
-
-    payload = "request"
-
-    response = requests.request("POST", url, json=payload, headers=headers)
-
-    print(response.text)
-    pass
-
-
-def process_message(sqs_message: str) -> None:
-    print(f"Processing message: {sqs_message}")
-    message = json.loads(sqs_message)
-    make_sendinblue_message(message["email"],message["name"])
-    # process your sqs message here
-    pass
-
 class Handler:
-    def __init__(self):
+    def __init__(self, key):
         self.received_signal = False
         signal(SIGINT, self._signal_handler)
         signal(SIGTERM, self._signal_handler)
+        self.key = key
 
     def _signal_handler(self, signal, frame):
         print(f"Handling signal {signal}, exiting gracefully")
         self.received_signal = True
+
+    def make_sendinblue_message(self,
+    email: str, name: str, email_template: str, email_subject: str
+) -> None:
+
+        url = "https://api.sendinblue.com/v3/smtp/email"
+
+        payload = {
+            "sender": {"name": "Gate", "email": "noreply@atdev.com"},
+            "to": [{"email": email, "name": name}],
+            "replyTo": {"email": "noreply@atdev.com", "name": "no-reply"},
+            "htmlContent": email_template,
+            "subject": email_subject,
+        }
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "api-key": self.key,
+        }
+
+        response = requests.request("POST", url, json=payload, headers=headers)
+
+        print(response.text)
+        pass
+
+
+    def process_message(self, sqs_message: str, email_template: str, email_subject: str) -> None:
+        print(f"Processing message: {sqs_message} with template {email_subject}: {email_template}")
+        message = json.loads(sqs_message)
+        self.make_sendinblue_message(message["email"], message["name"], email_template, email_subject)
+        # process your sqs message here
+        pass
+
+
 
 def wait(seconds: int):
     def decorator(fun):
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import boto3
+import configparser
 
-from handler import Handler, process_message, send_queue_metrics
+from handler import Handler, send_queue_metrics
 
-AWSID = "aws_secret_id"
-AWSSEC = "aws_secret_secret"
-REGION = "region"
-SQS_QUEUE_NAME = "queue-name"
-SQS_DEAD_LETTER_QUEUE_NAME = "queue-dead-letters"
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+print(config.sections())
+AWSID = config["aws"]["AWSID"]
+AWSSEC = config["aws"]["AWSSEC"]
+REGION = config["aws"]["REGION"]
+SQS_QUEUE_NAME = config["aws"]["SQS_QUEUE_NAME"]
+SQS_DEAD_LETTER_QUEUE_NAME = config["aws"]["SQS_DEAD_LETTER_QUEUE_NAME"]
+EMAIL_TEMPLATE = config["email"]["HTML"]
+EMAIL_SUBJECT = config["email"]["SUBJECT"]
+SENDIN_BLUE_KEY = config["email"]["APIKEY"]
 
 sqs = boto3.resource("sqs", aws_access_key_id = AWSID,
     aws_secret_access_key = AWSSEC,
@@ -15,16 +25,15 @@ queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_NAME)
 dlq = sqs.get_queue_by_name(QueueName=SQS_DEAD_LETTER_QUEUE_NAME)
 
 if __name__ == "__main__":
-    signal_handler = Handler()
+    signal_handler = Handler(SENDIN_BLUE_KEY)
     while not signal_handler.received_signal:
         send_queue_metrics(queue)
         send_queue_metrics(dlq)
         messages = queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=1,)
         for message in messages:
             try:
-                process_message(message.body)
+                signal_handler.process_message(message.body, EMAIL_TEMPLATE, EMAIL_SUBJECT)
             except Exception as e:
                 print(f"exception while processing message: {repr(e)}")
                 continue
-
             message.delete()
