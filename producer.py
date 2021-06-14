f552867dce2b1d6b0cc8a252d35486b0fb8a6402
andreituta <tuta.andrei96@gmail.com>
2021-06-14 13:57:40 +0100

    Create producer as a class and with method to generate a new message

--- producer.py
+++ producer.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import configparser
 
 config = configparser.ConfigParser()
@@ -10,17 +11,22 @@ AWSSEC = config["aws"]["AWSSEC"]
 REGION = config["aws"]["REGION"]
 SQS_QUEUE_NAME = config["aws"]["SQS_QUEUE_NAME"]
 SQS_DEAD_LETTER_QUEUE_NAME = config["aws"]["SQS_DEAD_LETTER_QUEUE_NAME"]
-EMAIL_TEMPLATE_OUT = config["email"]["HTML"]
-EMAIL_SUBJECT = config["email"]["SUBJECT"]
 SENDIN_BLUE_KEY = config["email"]["APIKEY"]
 
-sqs = boto3.resource("sqs", aws_access_key_id = AWSID,
-    aws_secret_access_key = AWSSEC,
-    region_name = REGION)
-queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_NAME)
+class SqsProducer():
+    def __init__(self, AWSID:str, AWSSEC:str, REGION:str, SQS_QUEUE_NAME:str):
+        sqs = boto3.resource("sqs", aws_access_key_id = AWSID,
+            aws_secret_access_key = AWSSEC,
+            region_name = REGION)
+        self.queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_NAME)
 
-# Create a new message
-response = queue.send_message(MessageBody=EMAIL_TEMPLATE_OUT)
+    def new_message(self, message:dict)->None:
+        # Create a new message
+        response = self.queue.send_message(MessageBody=json.dumps(message))
+        # The response is NOT a resource, but gives you a message ID and MD5
+        print(f'Uploaded a message with the id:{response.get("MessageId")}')
 
-# The response is NOT a resource, but gives you a message ID and MD5
-print(f'Uploaded a message with the id:{response.get("MessageId")}')
+
+if __name__ == "__main__":
+    producer=SqsProducer(AWSID, AWSSEC, REGION, SQS_QUEUE_NAME)
+    producer.new_message({"name": "Andrei","email": "tuta.andrei96@gmail.com" , "subject": "text to show as subject of the email","isContact": "False"})
